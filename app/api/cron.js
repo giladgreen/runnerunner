2772338e7f545dff4d99b6058a23f602e252a37e
gilad green <gilad.green@autodesk.com>
2024-06-22 22:40:31 +0300

    refactor

--- app/api/cron.js
+++ app/api/cron.js
@@ -3,7 +3,7 @@ import {removeOldRsvp} from "../lib/actions";
 export default function handler(req, res) {
     removeOldRsvp().then(() => {
         res.status(200).end('removed old rsvps');
-    }).catchall((err) => {
+    }).catch((err) => {
         console.log('error removing old rsvps', err);
         res.status(500).end('error removing old rsvps');
     });
