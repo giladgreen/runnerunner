c513982e3f7add768761ff661af9c8d51499d824
gilad green <gilad.green@autodesk.com>
2024-07-18 00:38:57 +0300

    Update RSVPButton.tsx

--- app/ui/client/RSVPButton.tsx
+++ app/ui/client/RSVPButton.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { experimental_useOptimistic as useOptimistic } from "react";
 
 import { PlayerDB } from '@/app/lib/definitions';
 import { rsvpPlayerForDay } from '@/app/lib/actions';
@@ -14,15 +15,22 @@ export default function RSVPButton({
   text?: string;
 }) {
   const prevPage = `${usePathname()}?${useSearchParams().toString()}`;
-
   const date = stringDate ?? new Date().toISOString().slice(0, 10);
+
   const isRsvpForDate = player.rsvps.includes(date);
-  const icon = isRsvpForDate ? '🟢' : '⚫️';
+
+  const [optimisticIsRsvpForDate, setOptimisticIsRsvpForDate] = useOptimistic(
+      isRsvpForDate,
+      (state, l) => !state
+  );
+  
+  const icon = optimisticIsRsvpForDate ? '🟢' : '⚫️';
   return (
     <div
-      onClick={() =>
-        rsvpPlayerForDay(player.phone_number, date, !isRsvpForDate, prevPage)
-      }
+      onClick={() => {
+         setOptimisticIsRsvpForDate(!isRsvpForDate);
+         rsvpPlayerForDay(player.phone_number, date, !isRsvpForDate, prevPage)
+      }}
     >
       <span className="pointer">{icon}</span> {text}
     </div>
