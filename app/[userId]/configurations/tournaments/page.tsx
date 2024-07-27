35c3b39f788d63a26b297802f58a7fea6dda864b
gilad green <gilad.green@autodesk.com>
2024-07-27 20:02:14 +0300

    Update page.tsx

--- app/[userId]/configurations/tournaments/page.tsx
+++ app/[userId]/configurations/tournaments/page.tsx
@@ -29,8 +29,8 @@ export default async function TournamentsSetupPage({
   const tournaments = await fetchTournaments();
 
   return (
-    <div className="config-section rtl" style={{ textAlign: 'right' }}>
-      <div>
+    <div className="config-section rtl" style={{ textAlign: 'right', marginRight: -20 }}>
+      <div style={{ textAlign: 'right', marginRight: 20 }}>
         <b>
           <u>טורנירים:</u>
         </b>
