6224e6e3227ed83d8bcb5c9c3e14d22dbbc43c49
gilad green <gilad.green@autodesk.com>
2024-08-02 15:39:14 +0300

    Update SignOutButton.tsx

--- app/ui/client/SignOutButton.tsx
+++ app/ui/client/SignOutButton.tsx
@@ -48,7 +48,7 @@ export default function SignOutButton({signOut, playerScreen }: { signOut: () =>
     },[idleTimeout, eventListener, showedAlert])
 
 
-    const disconnectText = isInside ? "התנתק" : "אל תשכח להתנתק לפני שאתה סוגר את הלשונית";
+    const disconnectText = playerScreen || isInside ? "התנתק" : "אל תשכח להתנתק לפני שאתה סוגר את הלשונית";
     return (
         <div>
             <button
@@ -60,7 +60,7 @@ export default function SignOutButton({signOut, playerScreen }: { signOut: () =>
                     "rtl flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-100 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
                     }
 
-                style={{ marginTop: playerScreen ? 5 : 0, background: idleTimeout < TIMEOUT_WARNING && isInside ? 'transparent' : (idleTimeout % 2 === 0 ? '#FF5555' : 'transparent')}}    >
+                style={{ marginTop: playerScreen ? 5 : 0, background: playerScreen ? 'transparent' : (idleTimeout < TIMEOUT_WARNING && isInside ? 'transparent' : (idleTimeout % 2 === 0 ? '#FF5555' : 'transparent'))}}    >
                 <ArrowRightOnRectangleIcon className={playerScreen ? "w-10" : "w-6"}/>
                 {!playerScreen && <div className="hidden md:block">{disconnectText}</div>}
                 {!playerScreen && idleTimeout > TIMEOUT_WARNING && idleTimeout > 0 && <div className="hidden md:block" style={{ margin:'0 20px'}}>({TIMEOUT - idleTimeout})</div>}
