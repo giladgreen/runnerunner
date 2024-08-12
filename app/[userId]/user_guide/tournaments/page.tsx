f10648bb2c877b97f46311079b8269acf67003cb
gilad green <gilad.green@autodesk.com>
2024-08-12 19:07:31 +0300

    ref

--- app/[userId]/user_guide/tournaments/page.tsx
+++ app/[userId]/user_guide/tournaments/page.tsx
@@ -1,6 +1,7 @@
 import {  fetchUserById } from '@/app/lib/data';
 import React from "react";
 import NoPermissionsPage from "@/app/ui/client/NoPermissionsPage";
+import Image from "next/image";
 
 export default async function UserGuideTournamentsPage({
   params,
@@ -15,7 +16,7 @@ export default async function UserGuideTournamentsPage({
         return <NoPermissionsPage />
     }
 
-    return <div className="rtl w-full" style={{ zoom: 1.3}}>
+    return <div className="rtl w-full" style={{zoom: 1.3}}>
         <div className="flex w-full items-center justify-between">
             <h1 className="text-2xl">
                 <b>
@@ -24,8 +25,46 @@ export default async function UserGuideTournamentsPage({
             </h1>
         </div>
         <div className="flex w-full items-center justify-between " style={{marginTop: 20}}>
-            TBD
+            על מנת לערוך טורנירים יש להיכנס לעמוד העריכה דרך עמוד ההגדרות:
         </div>
-
+        <div className="flex w-full items-center justify-between " style={{marginTop: 10}}>
+            <Image
+                src="/user_guide/tournaments/1.png"
+                width={700}
+                height={10}
+                alt="tournaments1"
+                style={{border: "1px solid black"}}
+            />
+        </div>
+        <div className="flex w-full items-center justify-between " style={{marginTop: 20}}>
+            בעמוד זה ניתן לראות את כל הטורנירים הקיימים,לערוך אותם, למחוק* אותם וליצור חדשים
+        </div>
+        <div className="flex w-full items-center justify-between " style={{marginTop: 10}}>
+            <Image
+                src="/user_guide/tournaments/2.png"
+                width={900}
+                height={10}
+                alt="tournaments2"
+                style={{border: "1px solid black"}}
+            />
+        </div>
+        <div className="flex w-full items-center justify-between " style={{marginTop: 20}}>
+            *שימו לב שניתן למחוק טורניר רק אם יש יותר מטורניר אחד באותו יום
+        </div>
+        <div className="flex w-full items-center justify-between " style={{marginTop: 20}}>
+            **על מנת לסמן טורניר קיים כלא פעיל יש לסמן שנדרש אישור הגעה ומספר השחקנים המקסימאלי הוא אפס
+        </div>
+        <div className="flex w-full items-center justify-between " style={{marginTop: 40}}>
+           עמוד יצירת טורניר חדש
+        </div>
+        <div className="flex w-full items-center justify-between " style={{marginTop: 10}}>
+            <Image
+                src="/user_guide/tournaments/3.png"
+                width={400}
+                height={10}
+                alt="tournaments3"
+                style={{border: "1px solid black"}}
+            />
+        </div>
     </div>
 }
