6430bf0ced4203888ccc6483efa0444efc87f4e3
gilad green <gilad.green@autodesk.com>
2024-09-04 17:44:25 +0300

    refactor

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -1,8 +1,78 @@
 export default async function TermsPage() {
-  return (
-    <div>
-      <h1>Terms of Service</h1>
-      TBD
-    </div>
+    const companyName = "Runner Runner";
+
+  return (<div className="terms-of-service">
+              <h1 className="header-h1">Terms of Service</h1>
+              <h2 className="header-h2"><u>Introduction</u></h2>
+              <p className="pera">Welcome to {companyName} ("Company", "we", "our", "us"). The following
+                  Terms of Service ("Terms", "Terms of Service") govern your use of our website located at (together or individually "Service") operated by {companyName}.</p>
+              <p className="pera">By accessing or using our Service, you agree to be bound by these Terms. If
+                  you disagree with any part of the terms, then you do not have permission to access the Service.</p>
+              <h2 className="header-h2"><u>Accounts</u></h2>
+              <p className="pera">When you create an account with us, you guarantee that you are above the age
+                  of 18 and that the information you provide us is accurate, complete, and current at all times.
+                  Inaccurate, incomplete, or obsolete information may result in the immediate termination of your
+                  account on the Service.</p>
+              <p className="pera">You are responsible for maintaining the confidentiality of your account and
+                  password, including but not limited to the restriction of access to your computer and/or account. You
+                  agree to accept responsibility for any and all activities or actions that occur under your account
+                  and/or password, whether your password is with our Service or a third-party service. You must notify
+                  us immediately upon becoming aware of any breach of security or unauthorized use of your account.</p>
+              <h2 className="header-h2"><u>Intellectual Property</u></h2>
+              <p className="pera">The Service and its original content (excluding Content provided by users),
+                  features, and functionality are and will remain the exclusive property of {companyName} and its
+                  licensors. The Service is protected by copyright, trademark, and other laws of both the country and
+                  foreign countries. Our trademarks and trade dress may not be used in connection with any product or
+                  service without the prior written consent of {companyName}.</p>
+              <h2 className="header-h2"><u>Links To Other Web Sites</u></h2>
+              <p className="pera">Our Service may contain links to third-party web sites or services that are
+                  not owned or controlled by {companyName}.</p>
+              <p className="pera">{companyName} has no control over, and assumes no responsibility for,
+                  the content, privacy policies, or practices of any third party web sites or services. We do not
+                  warrant the offerings of any of these entities/individuals or their websites.</p>
+              <p className="pera">You acknowledge and agree that {companyName} shall not be responsible
+                  or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in
+                  connection with the use of or reliance on any such content, goods, or services available on or through
+                  any such third-party web sites or services.</p>
+              <p className="pera">We strongly advise you to read the terms and conditions and privacy policies
+                  of any third-party web sites or services that you visit.</p>
+              <h2 className="header-h2"><u>Limitation Of Liability</u></h2>
+              <p className="pera">In no event shall {companyName}, nor its directors, employees,
+                  partners, agents, suppliers, or affiliates, be liable for any indirect, incidental, special,
+                  consequential, or punitive damages, including without limitation, loss of profits, data, use,
+                  goodwill, or other intangible losses, resulting from (i) your access to or use of or inability to
+                  access or use the Service; (ii) any conduct or content of any third party on the Service; (iii) any
+                  content obtained from the Service; and (iv) unauthorized access, use, or alteration of your
+                  transmissions or content, whether based on warranty, contract, tort (including negligence), or any
+                  other legal theory, whether or not we have been informed of the possibility of such damage, and even
+                  if a remedy set forth herein is found to have failed of its essential purpose.</p>
+              <h2 className="header-h2"><u>Disclaimer</u></h2>
+              <p className="pera">Your use of the Service is at your sole risk. The Service is provided on an
+                  "AS IS" and "AS AVAILABLE" basis. The Service is provided without warranties of any kind, whether
+                  express or implied, including, but not limited to, implied warranties of merchantability, fitness for
+                  a particular purpose, non-infringement, or course of performance.</p>
+              <p className="pera">{companyName} its subsidiaries, affiliates, and its licensors do not
+                  warrant that a) the Service will function uninterrupted, secure, or available at any particular time
+                  or location; b) any errors or defects will be corrected; c) the Service is free of viruses or other
+                  harmful components; or d) the results of using the Service will meet your requirements.</p>
+              <h2 className="header-h2"><u> Governing Law</u></h2>
+              <p className="pera">These Terms shall be governed and construed in accordance with the laws of
+                  Israel, without regard to its conflict of law provisions.</p>
+              <p className="pera">Our failure to enforce any right or provision of these Terms will not be
+                  considered a waiver of those rights. If any provision of these Terms is held to be invalid or
+                  unenforceable by a court, the remaining provisions of these Terms will remain in effect. These Terms
+                  constitute the entire agreement between us regarding our Service, and supersede and replace any prior
+                  agreements we might have had between us regarding the Service.</p>
+              <h2 className="header-h2"><u>Changes</u></h2>
+              <p className="pera">We reserve the right, at our sole discretion, to modify or replace these
+                  Terms at any time. If a revision is material we will provide at least 30 days notice prior to any new
+                  terms taking effect. What constitutes a material change will be determined at our sole discretion.</p>
+              <p className="pera">By continuing to access or use our Service after any revisions become
+                  effective, you agree to be bound by the revised terms. If you do not agree to the new terms, you are
+                  no longer authorized to use the Service.</p>
+              <h2 className="header-h2"><u> Contact Us</u></h2>
+              <p className="pera">If you have any questions about these Terms, please contact us at 050-887-4068.</p>
+
+      </div>
   );
 }
