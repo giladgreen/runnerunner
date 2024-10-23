26289c5afe363ffc03d13cd2a6bffda1d327c60e
gilad green <gilad.green@autodesk.com>
2024-10-23 13:24:07 +0300

    refactor

--- app/lib/cache.ts
+++ app/lib/cache.ts
@@ -1,12 +1,13 @@
 import { kv } from '@vercel/kv';
 import { UserDB } from '@/app/lib/definitions';
 
-export async function get(key: string, namespace: string = '') {
+export async function get(key: string, entityType: string = '') {
   try {
-    const result = await kv.hget<string>(`namespace-${namespace}`, key);
+    const result = await kv.hget<string>(`entity-type-${entityType}`, key);
     if (!result) {
       result;
     }
+    console.log(`found ${entityType} in cache. (key: ${key})`);
     return typeof result === 'string' ? JSON.parse(result as string) : result;
   } catch (e) {
     console.error(e);
@@ -14,20 +15,20 @@ export async function get(key: string, namespace: string = '') {
   }
 }
 
-export async function set(key: string, value: any, namespace: string = '') {
+export async function set(key: string, value: any, entityType: string = '') {
   try {
+    console.log(`saving ${entityType} to cache. (key: ${key})`);
     const data = typeof value === 'string' ? value : JSON.stringify(value);
-    console.log('data size', data.length);
-    return await kv.hset(`namespace-${namespace}`, { [key]: data });
+    return await kv.hset(`entity-type-${entityType}`, { [key]: data });
   } catch (e) {
     console.error(e);
     return null;
   }
 }
 
-export async function del(key: string, namespace: string = '') {
+export async function del(key: string, entityType: string = '') {
   try {
-    return await kv.del(`namespace-${namespace}`, key);
+    return await kv.del(`entity-type-${entityType}`, key);
   } catch (e) {
     console.error(e);
     return null;
@@ -35,12 +36,13 @@ export async function del(key: string, namespace: string = '') {
 }
 
 export async function getUserById(userId: string) {
-  return await get(userId, 'users');
+  return await get(userId, 'user');
 }
+
 export async function saveUser(user: UserDB) {
-  return await set(user.id, user, 'users');
+  return await set(user.id, user, 'user');
 }
 
 export async function removeUserById(userId: string) {
-  return await del(userId, 'users');
+  return await del(userId, 'user');
 }
