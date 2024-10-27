e48ca4631a71b4e816bb13b5fa93c310c519e84d
gilad green <gilad.green@autodesk.com>
2024-10-27 09:07:45 +0200

    refactor

---
+++ app/api/tournaments/route.ts
@@ -0,0 +1,11 @@
+import {fetchTournaments} from "@/app/lib/data";
+
+export async function GET(request: Request) {
+  try {
+    const tournaments = await fetchTournaments();
+    return Response.json({ tournaments });
+  } catch (error) {
+    console.error('fetchTournaments Error:', error);
+    return Response.json({ error });
+  }
+}
