75c18908917eb68bdddaa8deba8e8db17466c2f1
gilad green <gilad.green@autodesk.com>
2024-11-10 17:03:51 +0200

    refactor

--- app/ui/client/EntriesButton.tsx
+++ app/ui/client/EntriesButton.tsx
@@ -4,11 +4,12 @@ import { PlayerDB } from '@/app/lib/definitions';
 import { undoPlayerLastLog } from '@/app/lib/actions';
 import Image from 'next/image';
 import { usePathname, useSearchParams } from 'next/navigation';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import AreYouSure from '@/app/ui/client/AreYouSure';
 import Spinner from '@/app/ui/client/Spinner';
 import { Tooltip } from '@nextui-org/react';
 import { Tooltip as BlackTooltip } from 'flowbite-react';
+import Snackbar, {SnackbarCloseReason} from "@mui/material/Snackbar";
 
 const formatPlayerEntries = (
   entries: number,
@@ -16,7 +17,10 @@ const formatPlayerEntries = (
   prevEntriesTooltipText?: string[],
   undoTooltipText?: string,
 ) => {
-  if (isPending) {
+
+
+
+    if (isPending) {
     return <Spinner size={30} />;
   }
 
@@ -53,13 +57,18 @@ const formatPlayerEntries = (
       placement="bottom"
     >
       <BlackTooltip content={undoTooltipText} color="default">
-        <Image
-          src={`/${map[entries]}.png`}
-          alt={`players entries: ${entries}`}
-          className="zoom-on-hover mr-4"
-          width={35}
-          height={35}
-        />
+          <div>
+              <Image
+                  src={`/${map[entries]}.png`}
+                  alt={`players entries: ${entries}`}
+                  className="zoom-on-hover mr-4"
+                  width={35}
+                  height={35}
+              />
+
+          </div>
+
+
       </BlackTooltip>
     </Tooltip>
   );
@@ -75,7 +84,22 @@ export default function EntriesButton({
   const [showConfirmation, setShowConfirmation] = useState(false);
 
   const [isPending, setIsPending] = useState(false);
+    const [open, setOpen] = React.useState(false);
 
+    const handleClick = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (
+        event: React.SyntheticEvent | Event,
+        reason?: SnackbarCloseReason,
+    ) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
   const currentPage = `${usePathname()}?${useSearchParams().toString()}`;
   const entriesCount = player.entries ?? 0;
   return (
@@ -83,20 +107,39 @@ export default function EntriesButton({
       <div
         onClick={() => {
           setShowConfirmation(true);
+
         }}
         className="pointer"
       >
         {formatPlayerEntries(
           entriesCount,
-          isPending,
-          player.entriesTooltipText,
-          player.undoEntriesTooltipText,
+            isPending,
+            player.entriesTooltipText,
+            player.undoEntriesTooltipText,
+
         )}
       </div>
-      {showConfirmation && (
-        <AreYouSure
-          onConfirm={() => {
+        <Snackbar
+            className="only-wide-screen"
+            open={open}
+            autoHideDuration={2000}
+            onClose={handleClose}
+            message={
+                <span style={{fontSize: 18}}>
+                    כניסה אחרונה של
+                    <b style={{ marginLeft: 5, marginRight: 5}} >{player.name}</b>
+                     בוטלה
+          </span>
+            }
+
+
+        />
+
+        {showConfirmation && (
+            <AreYouSure
+                onConfirm={() => {
             setShowConfirmation(false);
+
             undoPlayerLastLog(player.phone_number, currentPage);
             setIsPending(true);
             const historyLogToRemove =
@@ -116,6 +159,7 @@ export default function EntriesButton({
                 arrived,
               });
               setIsPending(false);
+              handleClick();
             }, 1000);
           }}
           onCancel={() => {
